b167f133f9e54b85abad8b7013240107bb8cd18a
AtoMicKraK1n <prakharsharma2506@gmail.com>
2026-06-11 11:26:15 +0530

    fixing the redirecting

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,16 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: "/:path*",
+        has: [{ type: "host", value: "sentio-site-six.vercel.app" }],
+        destination: "https://sentiosecurity.xyz/:path*",
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
